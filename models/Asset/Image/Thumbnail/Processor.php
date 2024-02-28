c50e687627feceb4254dcb066c27457685d4afde
Dominik Labudzinski <dominik@labudzinski.com>
2024-02-28 14:11:12 +0100

    PSD preview

--- models/Asset/Image/Thumbnail/Processor.php
+++ models/Asset/Image/Thumbnail/Processor.php
@@ -17,6 +17,8 @@ declare(strict_types=1);
 namespace Pimcore\Model\Asset\Image\Thumbnail;
 
 use League\Flysystem\FilesystemException;
+use League\Flysystem\Local\LocalFilesystemAdapter;
+use Symfony\Component\Process\Process;
 use function ltrim;
 use function md5;
 use Pimcore\Config as PimcoreConfig;
@@ -172,115 +174,137 @@ class Processor
         $storagePath = $thumbDir . '/' . $filename;
         $storage = Storage::get('thumbnail');
 
-        // check for existing and still valid thumbnail
+        if($fileExt === 'psd') {
+            /** @var \League\Flysystem\Filesystem $storage */
+            $assetStorage = Storage::get('asset');
+            $assetStorageReflection = new \ReflectionClass($assetStorage);
+            /** @var LocalFilesystemAdapter $adapter */
+            $assetStorageAdapter = $assetStorageReflection->getProperty('adapter')->getValue($assetStorage);
+            $localAssetStoragePath = (new \ReflectionClass($assetStorageAdapter))->getProperty('rootLocation')->getValue($assetStorageAdapter);
 
-        $modificationDate = null;
-        $statusCacheEnabled = PimcoreConfig::getSystemConfiguration('assets')['image']['thumbnails']['status_cache'];
-        if ($statusCacheEnabled && $deferred) {
-            $modificationDate = $asset->getDao()->getCachedThumbnailModificationDate($config->getName(), $filename);
+            $storageReflection = new \ReflectionClass($storage);
+            /** @var LocalFilesystemAdapter $adapter */
+            $storageAdapter = $storageReflection->getProperty('adapter')->getValue($storage);
+            $localStoragePath = (new \ReflectionClass($storageAdapter))->getProperty('rootLocation')->getValue($storageAdapter);
+            $storage->createDirectory($thumbDir);
+
+            $process = Process::fromShellCommandline(
+                sprintf(
+                    'convert -limit memory 10MB -limit map 10mb %s -flatten -thumbnail 600x %s',
+                    $localAssetStoragePath.$asset->getRealFullPath(),
+                    $localStoragePath.$storagePath)
+            );
+            $processCode = $process->run();
         } else {
-            try {
-                $modificationDate = $storage->lastModified($storagePath);
-            } catch (FilesystemException $e) {
-                // nothing to do
-            }
-        }
 
-        if ($modificationDate) {
-            try {
-                if ($modificationDate >= $asset->getDataModificationDate()) {
-                    return [
-                        'src' => $storagePath,
-                        'type' => 'thumbnail',
-                        'storagePath' => $storagePath,
-                    ];
-                } else {
-                    // delete the file if it's not valid anymore, otherwise writing the actual data from
-                    // the local tmp-file to the real storage a bit further down doesn't work, as it has a
-                    // check for race-conditions & locking, so it needs to check for the existence of the thumbnail
-                    $storage->delete($storagePath);
-
-                    // refresh the thumbnail cache, if the asset modification date is modified
-                    // this is necessary because the thumbnail cache is not cleared automatically
-                    // when the original asset is modified
-                    $asset->getDao()->deleteFromThumbnailCache($config->getName());
-                }
-            } catch (FilesystemException $e) {
-                // nothing to do
-            }
-        }
-
-        // deferred means that the image will be generated on-the-fly (when requested by the browser)
-        // the configuration is saved for later use in
-        // \Pimcore\Bundle\CoreBundle\Controller\PublicServicesController::thumbnailAction()
-        // so that it can be used also with dynamic configurations
-        $pathInfo = ltrim($asset->getRealPath(), '/') . $asset->getId() . '/' . $config->getName() . '/' . $filename;
-        $tmpStoreDeferredConfigId = 'thumb_' . $asset->getId() . '__' . md5($pathInfo);
-        if ($deferred) {
-            // only add the config to the TmpStore if necessary (e.g. if the config is auto-generated)
-            if (!Config::exists($config->getName())) {
-                TmpStore::add($tmpStoreDeferredConfigId, $config, 'thumbnail_deferred');
-            }
-
-            return [
-                'src' => $storagePath,
-                'type' => 'deferred',
-                'storagePath' => $storagePath,
-            ];
-        }
-
-        // transform image
-        $image->setPreserveColor($config->isPreserveColor());
-        $image->setPreserveMetaData($config->isPreserveMetaData());
-        $image->setPreserveAnimation($config->getPreserveAnimation());
-
-        $fileExists = false;
-
-        try {
-            // check if file is already on the file-system and if it is still valid
-            $modificationDate = $storage->lastModified($storagePath);
-            if ($modificationDate < $asset->getModificationDate()) {
-                $storage->delete($storagePath);
+            // check for existing and still valid thumbnail
+            $modificationDate = null;
+            $statusCacheEnabled = PimcoreConfig::getSystemConfiguration('assets')['image']['thumbnails']['status_cache'];
+            if ($statusCacheEnabled && $deferred) {
+                $modificationDate = $asset->getDao()->getCachedThumbnailModificationDate($config->getName(), $filename);
             } else {
-                $fileExists = true;
+                try {
+                    $modificationDate = $storage->lastModified($storagePath);
+                } catch (FilesystemException $e) {
+                    // nothing to do
+                }
             }
-        } catch (\Exception $e) {
-            Logger::debug($e->getMessage());
-        }
 
-        if ($fileExists === false) {
-            $lockKey = 'image_thumbnail_' . $asset->getId() . '_' . md5($storagePath);
-            $lock = \Pimcore::getContainer()->get(LockFactory::class)->createLock($lockKey);
+            if ($modificationDate) {
+                try {
+                    if ($modificationDate >= $asset->getDataModificationDate()) {
+                        return [
+                            'src' => $storagePath,
+                            'type' => 'thumbnail',
+                            'storagePath' => $storagePath,
+                        ];
+                    } else {
+                        // delete the file if it's not valid anymore, otherwise writing the actual data from
+                        // the local tmp-file to the real storage a bit further down doesn't work, as it has a
+                        // check for race-conditions & locking, so it needs to check for the existence of the thumbnail
+                        $storage->delete($storagePath);
 
-            $lock->acquire(true);
+                        // refresh the thumbnail cache, if the asset modification date is modified
+                        // this is necessary because the thumbnail cache is not cleared automatically
+                        // when the original asset is modified
+                        $asset->getDao()->deleteFromThumbnailCache($config->getName());
+                    }
+                } catch (FilesystemException $e) {
+                    // nothing to do
+                }
+            }
 
-            $startTime = microtime(true);
-
-            // after we got the lock, check again if the image exists in the meantime - if not - generate it
-            if (!$storage->fileExists($storagePath)) {
-                // all checks on the file system should be below the deferred part for performance reasons (remote file systems)
-                if (!$fileSystemPath) {
-                    $fileSystemPath = $asset->getLocalFile();
+            // deferred means that the image will be generated on-the-fly (when requested by the browser)
+            // the configuration is saved for later use in
+            // \Pimcore\Bundle\CoreBundle\Controller\PublicServicesController::thumbnailAction()
+            // so that it can be used also with dynamic configurations
+            $pathInfo = ltrim($asset->getRealPath(), '/') . $asset->getId() . '/' . $config->getName() . '/' . $filename;
+            $tmpStoreDeferredConfigId = 'thumb_' . $asset->getId() . '__' . md5($pathInfo);
+            if ($deferred) {
+                // only add the config to the TmpStore if necessary (e.g. if the config is auto-generated)
+                if (!Config::exists($config->getName())) {
+                    TmpStore::add($tmpStoreDeferredConfigId, $config, 'thumbnail_deferred');
                 }
 
-                if (is_resource($fileSystemPath)) {
-                    $fileSystemPathStream = $fileSystemPath;
-                    $fileSystemPath = self::getLocalFileFromStream($fileSystemPath);
-                    @fclose($fileSystemPathStream);
+                return [
+                    'src' => $storagePath,
+                    'type' => 'deferred',
+                    'storagePath' => $storagePath,
+                ];
+            }
+
+            // transform image
+            $image->setPreserveColor($config->isPreserveColor());
+            $image->setPreserveMetaData($config->isPreserveMetaData());
+            $image->setPreserveAnimation($config->getPreserveAnimation());
+
+            $fileExists = false;
+
+            try {
+                // check if file is already on the file-system and if it is still valid
+                $modificationDate = $storage->lastModified($storagePath);
+                if ($modificationDate < $asset->getModificationDate()) {
+                    $storage->delete($storagePath);
+                } else {
+                    $fileExists = true;
                 }
+            } catch (\Exception $e) {
+                Logger::debug($e->getMessage());
+            }
 
-                if (!file_exists($fileSystemPath)) {
-                    throw new \Exception(sprintf('Source file %s does not exist!', $fileSystemPath));
-                }
+            if ($fileExists === false) {
+                $lockKey = 'image_thumbnail_' . $asset->getId() . '_' . md5($storagePath);
+                $lock = \Pimcore::getContainer()->get(LockFactory::class)->createLock($lockKey);
 
-                if (!$image->load($fileSystemPath, ['asset' => $asset])) {
-                    throw new \Exception(sprintf('Unable to generate thumbnail for asset %s from source image %s', $asset->getId(), $fileSystemPath));
-                }
+                $lock->acquire(true);
 
-                $transformations = $config->getItems();
+                $startTime = microtime(true);
 
-                // check if the original image has an orientation exif flag
-                // if so add a transformation at the beginning that rotates and/or mirrors the image
+                // after we got the lock, check again if the image exists in the meantime - if not - generate it
+                if (!$storage->fileExists($storagePath)) {
+                    // all checks on the file system should be below the deferred part for performance reasons (remote file systems)
+                    if (!$fileSystemPath) {
+                        $fileSystemPath = $asset->getLocalFile();
+                    }
+
+                    if (is_resource($fileSystemPath)) {
+                        $fileSystemPathStream = $fileSystemPath;
+                        $fileSystemPath = self::getLocalFileFromStream($fileSystemPath);
+                        @fclose($fileSystemPathStream);
+                    }
+
+                    if (!file_exists($fileSystemPath)) {
+                        throw new \Exception(sprintf('Source file %s does not exist!', $fileSystemPath));
+                    }
+
+                    if (!$image->load($fileSystemPath, ['asset' => $asset])) {
+                        throw new \Exception(sprintf('Unable to generate thumbnail for asset %s from source image %s', $asset->getId(), $fileSystemPath));
+                    }
+
+                    $transformations = $config->getItems();
+
+                    // check if the original image has an orientation exif flag
+                    // if so add a transformation at the beginning that rotates and/or mirrors the image
 //                if (function_exists('exif_read_data')) {
 //                    try {
 //                        $exif = @exif_read_data($fileSystemPath);
@@ -330,44 +354,45 @@ class Processor
 //                    } catch (\Throwable $ignored) {}
 //                }
 
-                self::applyTransformations($image, $asset, $config, $transformations);
+                    self::applyTransformations($image, $asset, $config, $transformations);
 
-                if ($optimizedFormat) {
-                    $format = $image->getContentOptimizedFormat();
+                    if ($optimizedFormat) {
+                        $format = $image->getContentOptimizedFormat();
+                    }
+
+                    $tmpFsPath = File::getLocalTempFilePath($fileExtension);
+                    $image->save($tmpFsPath, $format, $config->getQuality());
+                    $stream = fopen($tmpFsPath, 'rb');
+                    $storage->writeStream($storagePath, $stream);
+                    if (is_resource($stream)) {
+                        fclose($stream);
+                    }
+
+                    if ($statusCacheEnabled && $asset instanceof Asset\Image) {
+                        //update thumbnail dimensions to cache
+                        $asset->addThumbnailFileToCache($tmpFsPath, $filename, $config);
+                    }
+
+                    if (!Config::exists($config->getName())) {
+                        // delete dynamic thumbnail configs out of the TmpStore as soon as we've generated the thumbnail file
+                        TmpStore::delete($tmpStoreDeferredConfigId);
+                    }
+
+                    $generated = true;
+
+                    $isImageOptimizersEnabled = PimcoreConfig::getSystemConfiguration('assets')['image']['thumbnails']['image_optimizers']['enabled'];
+                    if ($optimizedFormat && $optimizeContent && $isImageOptimizersEnabled) {
+                        \Pimcore::getContainer()->get('messenger.bus.pimcore-core')->dispatch(
+                            new OptimizeImageMessage($storagePath)
+                        );
+                    }
+
+                    Logger::debug('Thumbnail ' . $storagePath . ' generated in ' . (microtime(true) - $startTime) . ' seconds');
+                } else {
+                    Logger::debug('Thumbnail ' . $storagePath . ' already generated, waiting on lock for ' . (microtime(true) - $startTime) . ' seconds');
                 }
-
-                $tmpFsPath = File::getLocalTempFilePath($fileExtension);
-                $image->save($tmpFsPath, $format, $config->getQuality());
-                $stream = fopen($tmpFsPath, 'rb');
-                $storage->writeStream($storagePath, $stream);
-                if (is_resource($stream)) {
-                    fclose($stream);
-                }
-
-                if ($statusCacheEnabled && $asset instanceof Asset\Image) {
-                    //update thumbnail dimensions to cache
-                    $asset->addThumbnailFileToCache($tmpFsPath, $filename, $config);
-                }
-
-                if (!Config::exists($config->getName())) {
-                    // delete dynamic thumbnail configs out of the TmpStore as soon as we've generated the thumbnail file
-                    TmpStore::delete($tmpStoreDeferredConfigId);
-                }
-
-                $generated = true;
-
-                $isImageOptimizersEnabled = PimcoreConfig::getSystemConfiguration('assets')['image']['thumbnails']['image_optimizers']['enabled'];
-                if ($optimizedFormat && $optimizeContent && $isImageOptimizersEnabled) {
-                    \Pimcore::getContainer()->get('messenger.bus.pimcore-core')->dispatch(
-                        new OptimizeImageMessage($storagePath)
-                    );
-                }
-
-                Logger::debug('Thumbnail ' . $storagePath . ' generated in ' . (microtime(true) - $startTime) . ' seconds');
-            } else {
-                Logger::debug('Thumbnail ' . $storagePath . ' already generated, waiting on lock for ' . (microtime(true) - $startTime) . ' seconds');
+                $lock->release();
             }
-            $lock->release();
         }
 
         // quick bugfix / workaround, it seems that imagemagick / image optimizers creates sometimes empty PNG chunks (total size 33 bytes)
@@ -428,7 +453,7 @@ class Processor
                             if ($position !== false) {
                                 // high res calculations if enabled
                                 if (!in_array($transformation['method'], ['cropPercent']) && in_array($key,
-                                    ['width', 'height', 'x', 'y'])) {
+                                        ['width', 'height', 'x', 'y'])) {
                                     if ($highResFactor && $highResFactor > 1) {
                                         $value *= $highResFactor;
                                         $value = (int)ceil($value);
